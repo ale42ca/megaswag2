233c153b6bf98d2ba2df954dfc1af1b52736d772
ale42ca <59265646+ale42ca@users.noreply.github.com>
2019-12-30 18:54:52 +0100

    Update execute.php

--- execute.php
+++ execute.php
@@ -57,7 +57,7 @@ $dataoggi = getdataoggi($datazioneunix);
   $queryusername = $query['from']['username'];
   $querydata = $query['data'];
   $querymsgid = $query['message']['message_id'];
-
+$msgcanale="fico";
 switch ($testo) {
     case "/start":
         $ms = "ciao";
@@ -67,7 +67,8 @@ switch ($testo) {
     case "prenota":
         $ms = "prenotiamo lo studio";
 	sendMessage($utente, $ms);
-	inviamessaggiocanale($ms);
+	inviamessaggiocanale($msgcanale);	
+
         break;
     case "vedi prenotazioni":
         $ms = "chi ha prenotato lo studio nell' ultima settimana?";
@@ -92,8 +93,11 @@ switch ($testo) {
 	$ms = "benvenuto admin";
 	sendMessage($utente, $ms);
 		
-	comandiadmin($utente);
-        break;		
+	comandiadmin($utente,$testo);
+        break;
+    case "esci":	
+	tastierastart($utente);	
+   	break;	
     default:
         $ms = "non ho capito";
 	sendMessage($utente, $ms);
@@ -101,7 +105,9 @@ switch ($testo) {
 if($querydata == "ModificaMessaggio"){
     editMessageText($queryUserId,$querymsgid,"HEYLA!");
     exit();
-}	
+}
+
+	
 function tastierastart($utente){
 	$messaggio = "osserva la tastiera e usa i suoi comandi";
     	$tastiera = '&reply_markup={"keyboard":[["prenota"],["calendario"],["vedi prenotazioni"],["data"]]}';
@@ -113,42 +119,73 @@ function tastieracalendario($utente,$dataoggi){
    	
     $tastiera = '&reply_markup={"inline_keyboard":[[{"text":"1","callback_data":"Prenota"},{"text":"2","callback_data":"Prenota"},{"text":"3","callback_data":"Prenota"},{"text":"4","callback_data":"Prenota"},{"text":"5","callback_data":"Prenota"},{"text":"6","callback_data":"Prenota"},{"text":"7","callback_data":"Prenota"}]]}';
     $tastiera2 = '&reply_markup={"inline_keyboard":[[{"text":"8","callback_data":"Prenota"},{"text":"9","callback_data":"Prenota"},{"text":"10","callback_data":"Prenota"},{"text":"11","callback_data":"Prenota"},{"text":"12","callback_data":"Prenota"},{"text":"13","callback_data":"Prenota"},{"text":"14","callback_data":"Prenota"}]]}';
+
     $url = $GLOBALS[completo].'/sendMessage?chat_id='.$utente.'&parse_mod=HTML&text='.$message.$tastiera;
     $url2 = $GLOBALS[completo].'/sendMessage?chat_id='.$utente.'&parse_mod=HTML'.$message.$tastiera2;	
-    file_get_contents($url,$url2);
+    file_get_contents($url);
+
 }
 function sendMessage($utente, $msg){
 		$url = $GLOBALS[completo]."/sendMessage?chat_id=".$utente."&text=".urlencode($msg);
 		file_get_contents($url);
 }
-function getdataoggi($datamessaggio){
-  $datazioneunix = gmdate("d.m.y", $datamessaggio);
-  
-  return $datazioneunix;
-}
-  function editMessageText($chatId,$message_id,$newText)
-  {
+
+function editMessageText($chatId,$message_id,$newText){
     $url = $GLOBALS[completo]."/editMessageText?chat_id=$chatId&message_id=$message_id&parse_mode=HTML&text=".urlencode($newText);
     file_get_contents($url);
   }
-function comandiadmin($utente){
+
+function inviamessaggiocanale($msg){
+	$utente = "@santacaterina2";
+	$url = $GLOBALS[completo]."/sendMessage?chat_id=".$utente."&text=".urlencode($msg);
+	file_get_contents($url);
+}
+
+function comandiadmin($utente,$testoadmin){
 	$messaggio = "cosa vuole fare admin?";
-    	$tastiera = '&reply_markup={"keyboard":[["crea evento"],["assemblea"],["manda notifica"],["esci da admin"]]}';
+    	$tastiera = '&reply_markup={"keyboard":[["crea evento"],["assemblea"],["manda notifica"],["esci"]]}';
 	$url = "$GLOBALS[completo]"."/sendMessage?chat_id=".$utente."&parse_mode=HTML&text=".$messaggio.$tastiera;
 	file_get_contents($url);
-	if($testo == "assemblea"){
-		$msg="nuova assemblea"
-		inviamessaggiocanale($msg);
-		}
+		
+	if($testoadmin == "crea evento"){
+		$ms = "certamente";
+		sendMessage($utente, $ms);
+		exit();
+	}
+/*
+	switch ($testoadmin) {
+    		case "crea evento":
+        	$ms = "certamente";
+		sendMessage($admin, $ms);
+		
+        	break;
+		case "assemblea":
+		$ms = "quando vuole fare l' assemblea";
+		$msgcanale="prossima assemblea";
+		sendMessage($admin, $ms);
+		inviamessaggiocanale($msgcanale);	
 
-}
-function inviamessaggiocanale($msgcanale){
-	$utente = "@santacaterina2";
+		break;
+		case "manda notifica":
+		$ms = "notifica inviata";
+		$msgcanale="allert";
+		sendMessage($admin, $ms);
+		inviamessaggiocanale($msgcanale);	
+
+		break;			
+    		case "esci":	
+		tastierastart($utente);	
+   		break;
+	}
+*/	
 	
-	$url = $GLOBALS[completo]."/sendMessage?chat_id=".$utente."&text=".urlencode($msgcanale);
-	file_get_contents($url);
 }
-	
+//data
+function getdataoggi($datamessaggio){
+  $datazioneunix = gmdate("d.m.y", $datamessaggio);
+  return $datazioneunix;
+}
+
 
 //header("Content-Type: application/json");
 //$msg="vuoi fare altro?"; 
