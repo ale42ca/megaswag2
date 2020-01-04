f8cb3cca1c4fec6114e2ff4dc946842477dd90ec
ale42ca <59265646+ale42ca@users.noreply.github.com>
2020-01-04 23:13:50 +0100

    Update execute.php

--- execute.php
+++ execute.php
@@ -7,44 +7,8 @@ $token="872839539:AAGgmCXaX9zdSypFKiR4BHxoVK3U-riq3ao";
 $completo="https://api.telegram.org/bot".$token;
 $updates=file_get_contents("php://input");
 $update=json_decode($updates, true);
-function is_new_request($requestUpdateId)
-{
-    $filename = "./last_update_id.txt";
-    if (filesize($filename)) {
-        $file = fopen($filename, "w");
-        if ($file) {
-            fwrite($file, $requestUpdateId);
-            fclose($file);
-            return true;
-        } else
-            return null;
-    } else {
-        $file = fopen($filename, "w");
-        fwrite($file, 1);
-        fclose($file);
-        return false;
-    }
-}
-function set_get_updates_parameters($getUpdates)
-{
-    $filename = "./last_update_id.txt";
-    if (file_exists($filename)) {
-        $file = fopen($filename, "r");
-        $lastUpdateId = fgets($file);
-        fclose($file);
-    } else {
-        $file = fopen($filename, "w");
-        $lastUpdateId = fwrite($file, 1);
-        fclose($file);
-    }
-    return str_replace("100", $lastUpdateId, $getUpdates);
-}
 $updates = json_decode(file_get_contents(set_get_updates_parameters("https://api.telegram.org/bot872839539:AAGgmCXaX9zdSypFKiR4BHxoVK3U-riq3ao/getUpdates?offset=100")), true);
-// Separate every update in $updates
-$isNewRequest = is_new_request($update["update_id"]); // $update["update_id"] is update_id of one of your requests; e.g. 591019242
-if ($isNewRequest === false || $isNewRequest === null){
-	exit;	
-	}
+
 elseif(!$update){
   exit;
 }
@@ -238,6 +202,12 @@ function comandiadmin($utente){
 	
 }
 //data
+
+$db = pg_connect("host= ec2-54-247-96-169.eu-west-1.compute.amazonaws.com port=5432 dbname=d2hsht934ovhs9 user=maghsyclqxkpyw password=50ac10525450c60de9157e57e0ab6432f320f5ef3d8ee1650818e491644f51bc");
+$query = "INSERT INTO prenotazionistudio VALUES ('$_POST[username]','$_POST[data]',
+'$_POST[ora]','$_POST[created_on]')";
+$result = pg_query($query); 
+
 function getdataoggi($datamessaggio){
   $datazioneunix = gmdate("d.m.y", $datamessaggio);
   return $datazioneunix;
