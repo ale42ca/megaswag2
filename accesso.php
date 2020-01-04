ad7eb2fa878987c674345f189112f1e2be931964
ale42ca <59265646+ale42ca@users.noreply.github.com>
2020-01-04 17:09:30 +0100

    Create accesso.php

---
+++ accesso.php
@@ -0,0 +1,4 @@
+<?php
+pg_connect("host= ec2-54-247-96-169.eu-west-1.compute.amazonaws.com port=5432 dbname=d2hsht934ovhs9 user=maghsyclqxkpyw password=50ac10525450c60de9157e57e0ab6432f320f5ef3d8ee1650818e491644f51bc");
+
+?>
