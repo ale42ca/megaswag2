58597c518d29695193b6d558dc7658b4ce1b8303
ale42ca <59265646+ale42ca@users.noreply.github.com>
2020-01-06 00:32:46 +0100

    Update execute.php

--- execute.php
+++ execute.php
@@ -4,7 +4,47 @@ $token="872839539:AAGgmCXaX9zdSypFKiR4BHxoVK3U-riq3ao";
 $completo="https://api.telegram.org/bot".$token;
 $updates=file_get_contents("php://input");
 $update=json_decode($updates, true);
-
+function is_new_request($requestUpdateId)
+{
+    $filename = "./last_update_id.txt";
+    if (filesize($filename)) {
+        $file = fopen($filename, "w");
+        if ($file) {
+            fwrite($file, $requestUpdateId);
+            fclose($file);
+            return true;
+        } else
+            return null;
+    } else {
+        $file = fopen($filename, "w");
+        fwrite($file, 1);
+        fclose($file);
+        return false;
+    }
+}
+function set_get_updates_parameters($getUpdates)
+{
+    $filename = "./last_update_id.txt";
+    if (file_exists($filename)) {
+        $file = fopen($filename, "r");
+        $lastUpdateId = fgets($file);
+        fclose($file);
+    } else {
+        $file = fopen($filename, "w");
+        $lastUpdateId = fwrite($file, 1);
+        fclose($file);
+    }
+    return str_replace("100", $lastUpdateId, $getUpdates);
+}
+$updates = json_decode(file_get_contents(set_get_updates_parameters("https://api.telegram.org/bot872839539:AAGgmCXaX9zdSypFKiR4BHxoVK3U-riq3ao/getUpdates?offset=100")), true);
+// Separate every update in $updates
+$isNewRequest = is_new_request($update["update_id"]); // $update["update_id"] is update_id of one of your requests; e.g. 591019242
+if ($isNewRequest === false || $isNewRequest === null){
+	exit;	
+	}
+elseif(!$update){
+  exit;
+}
 
 $messaggio=$update['message'];
 $message_id=$update['message']['message_id'];
@@ -14,14 +54,12 @@ $utente=$messaggio['chat']['id'];
 $datazioneunix=$messaggio['date'];
 $dataoggi = getdataoggi($datazioneunix);
 $nomeutente=$messaggio['chat']['first_name'];
-  
-$query = $update['callback_query'];
-$queryid = $query['id'];
-$queryUserId = $query['from']['id'];
-$queryusername = $query['from']['username'];
-$querydata = $query['data'];
-$querymsgid = $query['message']['message_id'];
-
+  $query = $update['callback_query'];
+  $queryid = $query['id'];
+  $queryUserId = $query['from']['id'];
+  $queryusername = $query['from']['username'];
+  $querydata = $query['data'];
+  $querymsgid = $query['message']['message_id'];
 
 
 
