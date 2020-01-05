be3ba53b69c3f1d3f0dce9393a9050d16022cca7
ale42ca <59265646+ale42ca@users.noreply.github.com>
2020-01-05 22:57:59 +0100

    Update execute.php

--- execute.php
+++ execute.php
@@ -109,7 +109,7 @@ function database($nomeutente,$dataoggi){
 	$ora= "10:20";
 	
 	$db =pg_connect("host= ec2-54-247-96-169.eu-west-1.compute.amazonaws.com port=5432 dbname=d2hsht934ovhs9 user=maghsyclqxkpyw password=50ac10525450c60de9157e57e0ab6432f320f5ef3d8ee1650818e491644f51bc");
-	$query = "INSERT INTO  VALUES ('$nomeutente','$dataoggi',
+	$query = "INSERT INTO prenotazionistudi VALUES ('$nomeutente','$dataoggi',
 	'$ora','$dataoggi')";
 	$result = pg_query($query); 
 	
