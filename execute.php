2e45c2b86ffd698a83a98bb47e4b1e7d266f13ff
ale42ca <59265646+ale42ca@users.noreply.github.com>
2020-01-05 22:02:19 +0100

    Update execute.php

--- execute.php
+++ execute.php
@@ -206,7 +206,7 @@ function databasez($nomeutente,$dataoggi){
 	$ora= "10:21";
 	$quack="santa paperella";
 	$db =pg_connect("host= ec2-54-247-96-169.eu-west-1.compute.amazonaws.com port=5432 dbname=d2hsht934ovhs9 user=maghsyclqxkpyw password=50ac10525450c60de9157e57e0ab6432f320f5ef3d8ee1650818e491644f51bc");
-	$query = "INSERT INTO prenotazionistudi  VALUES ('$quack','$quack','$quack','$quack')";
+	$query = "INSERT INTO prenotazionistudi(data, username, ora)  VALUES ('$quack','$quack','$quack')";
 	$result = pg_query($query);
 
 if (!$result) {
