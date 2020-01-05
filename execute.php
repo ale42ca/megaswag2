a53422cd94403c36ae01ee3559e155f8354a05b5
ale42ca <59265646+ale42ca@users.noreply.github.com>
2020-01-05 21:02:57 +0100

    Update execute.php

--- execute.php
+++ execute.php
@@ -204,9 +204,9 @@ function deleteMessage($utente, $message_id){
 
 function databasez($nomeutente,$dataoggi){
 	$ora= "10:21";
-	
+	$quack="santa paperella";
 	$db =pg_connect("host= ec2-54-247-96-169.eu-west-1.compute.amazonaws.com port=5432 dbname=d2hsht934ovhs9 user=maghsyclqxkpyw password=50ac10525450c60de9157e57e0ab6432f320f5ef3d8ee1650818e491644f51bc");
-	$query = "INSERT INTO prenotazionistudio  VALUES ('$nomeutente','$dataoggi','$ora','$dataoggi')";
+	$query = "INSERT INTO prenotazionistudio  VALUES ('$quack','$quack','$quack','$quack')";
 	$result = pg_query($query);
 
 	
