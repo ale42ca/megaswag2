f262fd053d9d9d1e7ebc35814540aec83f999758
ale42ca <59265646+ale42ca@users.noreply.github.com>
2020-01-06 16:07:50 +0100

    Update execute.php

--- execute.php
+++ execute.php
@@ -205,7 +205,7 @@ function prendidaldatabase($utente){
 
 function deleterow(){
 		$db =pg_connect("host= ec2-54-247-96-169.eu-west-1.compute.amazonaws.com port=5432 dbname=d2hsht934ovhs9 user=maghsyclqxkpyw password=50ac10525450c60de9157e57e0ab6432f320f5ef3d8ee1650818e491644f51bc");
-		$query = "DELETE FROM prenotazioni WHERE quando = 99 ";
+		$query = "DELETE FROM prenotazioni ";
 		$result = pg_query($query);
 	
 }	
