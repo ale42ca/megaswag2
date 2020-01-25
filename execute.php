7c12418baf8d1e623aa6c02190c32a6f278f15eb
ale42ca <59265646+ale42ca@users.noreply.github.com>
2020-01-25 21:22:41 +0100

    Update execute.php

--- execute.php
+++ execute.php
@@ -5,38 +5,7 @@ $token="872839539:AAGgmCXaX9zdSypFKiR4BHxoVK3U-riq3ao";
 $completo="https://api.telegram.org/bot".$token;
 $updates=file_get_contents("php://input");
 $update=json_decode($updates, true);
-function is_new_request($requestUpdateId)
-{
-    $filename = "./last_update_id.txt";
-    if (filesize($filename)) {
-        $file = fopen($filename, "w");
-        if ($file) {
-            fwrite($file, $requestUpdateId);
-            fclose($file);
-            return true;
-        } else
-            return null;
-    } else {
-        $file = fopen($filename, "w");
-        fwrite($file, 1);
-        fclose($file);
-        return false;
-    }
-}
-function set_get_updates_parameters($getUpdates)
-{
-    $filename = "./last_update_id.txt";
-    if (file_exists($filename)) {
-        $file = fopen($filename, "r");
-        $lastUpdateId = fgets($file);
-        fclose($file);
-    } else {
-        $file = fopen($filename, "w");
-        $lastUpdateId = fwrite($file, 1);
-        fclose($file);
-    }
-    return str_replace("100", $lastUpdateId, $getUpdates);
-}
+
 $updates = json_decode(file_get_contents(set_get_updates_parameters("https://api.telegram.org/bot872839539:AAGgmCXaX9zdSypFKiR4BHxoVK3U-riq3ao/getUpdates?offset=100")), true);
 // Separate every update in $updates
 $isNewRequest = is_new_request($update["update_id"]); // $update["update_id"] is update_id of one of your requests; e.g. 591019242
@@ -63,58 +32,17 @@ $queryUserId = $query['from']['id'];
 $queryusername = $query['from']['username'];
 $querydata = $query['data'];
 $querymsgid = $query['message']['message_id'];
-
+// collegamento al database 
+$db =pg_connect("host= ec2-54-247-96-169.eu-west-1.compute.amazonaws.com port=5432 dbname=d2hsht934ovhs9 user=maghsyclqxkpyw password=50ac10525450c60de9157e57e0ab6432f320f5ef3d8ee1650818e491644f51bc");
 //start
 switch ($testo) {
     case "/start":
-	$ms = "Ciao sono Beecky assistente virtuale di radio frequenza libera. Cosa posso fare per te?";
+	$comando = '1';
+	prendidaldatabase($comando);
 	sendMessage($utente, $ms);	
         tastierastart($utente);	
         break;
-    case "prenota":
-        $ms = "prenotiamo lo studio";
-	sendMessage($utente, $ms);
-	$ms = "Mi serve che tu mi dica quando vuoi prenotarlo";
-	
-	sendMessage($utente, $ms);
-	//prenotazione();
-	$ms = "per che ora?";
-	sendMessage($utente, $ms);	
-	$dataprenotata="oggi";
-	//controllo conflitti
-	inserireneldatabase($utente,$dataoggi);	
-	//conferma e upload nel file	
-	$ms = "Perfetto! ora invio una notifica nel gruppo";
-	sendMessage($utente, $ms);
-	$msgcanale= "lo studio è stato prenotato ".$dataprenotata." da ".$nomeutente;	
-	inviamessaggiocanale($msgcanale);		
-        break;
-		
-    	case "vedi prenotazioni":
-        $ms = "chi ha prenotato lo studio in questa  settimana?";
-	sendMessage($utente, $ms);
-	//vediprenotazioni();	
-	prendidaldatabase($utente);
-        break;
-    case "calendario":
-        $ms = "vediamoun po'.... se non ricordo male oggi è";
-	sendMessage($utente, $ms);
-	$mesecalendario=date("m"); 	
-	tastieracalendario($utente,$dataoggi,$mesecalendario);
-        break;
-		
-    case "ciao":
-        $ms = "ciao, come stai?";
-	sendMessage($utente, $ms);
-	$ms = "Sai sono sempre impegnata, ma visto che sei così gentile ti racconto una barzelletta";
-	sendMessage($utente, $ms);	
-	break;
-    case "data":
-	$ms = "Oggi è";
-	sendMessage($utente, $ms);
-        sendMessage($utente, $dataoggi);
-	deleterow();	
-        break;
+   
     case "1admin":
 	$ms = "benvenuto admin";
 	sendMessage($utente, $ms);
@@ -210,13 +138,13 @@ function deleteMessage($utente, $message_id){
 }
 //inserire dati nel database
 function inserireneldatabase($utente,$dataoggi){
-	$db =pg_connect("host= ec2-54-247-96-169.eu-west-1.compute.amazonaws.com port=5432 dbname=d2hsht934ovhs9 user=maghsyclqxkpyw password=50ac10525450c60de9157e57e0ab6432f320f5ef3d8ee1650818e491644f51bc");
+
 	$query = "INSERT INTO prenotazioni (nome, quando, ora) VALUES ('$utente', '099','$dataoggi')";
 	$result = pg_query($query);
 }
 //get dati dal database
 function prendidaldatabase($utente){
-	$db =pg_connect("host= ec2-54-247-96-169.eu-west-1.compute.amazonaws.com port=5432 dbname=d2hsht934ovhs9 user=maghsyclqxkpyw password=50ac10525450c60de9157e57e0ab6432f320f5ef3d8ee1650818e491644f51bc");
+
 	$result = pg_query($db,"SELECT nome, quando, ora FROM prenotazioni  "); //WHERE quando = '099'
 	
 	while($row=pg_fetch_assoc($result)){
@@ -225,9 +153,21 @@ function prendidaldatabase($utente){
 		file_get_contents($url);	
 	}
 }
+// prendi text
+function prendidaldatabase($comando){
+
+
+	$result = pg_query($db,"SELECT comando FROM comandi WHERE numero = $comando   "); 
+	
+	while($row=pg_fetch_assoc($result)){
+		$msg=$row['comando'] ;
+		$url = $GLOBALS[completo]."/sendMessage?chat_id=".$utente."&text=".urlencode($msg);
+		file_get_contents($url);	
+	}
+}
 //cancella database
 function deleterow(){
-		$db =pg_connect("host= ec2-54-247-96-169.eu-west-1.compute.amazonaws.com port=5432 dbname=d2hsht934ovhs9 user=maghsyclqxkpyw password=50ac10525450c60de9157e57e0ab6432f320f5ef3d8ee1650818e491644f51bc");
+
 		$query = "DELETE FROM prenotazioni ";
 		$result = pg_query($query);
 	
