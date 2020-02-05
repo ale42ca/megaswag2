2c29f2160ff2081cc2e1fe843414d1e9cee44e97
ale42ca <59265646+ale42ca@users.noreply.github.com>
2020-02-05 23:35:43 +0100

    Update execute.php

--- execute.php
+++ execute.php
@@ -1,8 +1,5 @@
 <?php
-//beecky
-
-// webhook token e update che ci permettono di interagire con il bot
-$web ="https://api.telegram.org/bot";
+$web="https://api.telegram.org/bot";
 $token="872839539:AAGgmCXaX9zdSypFKiR4BHxoVK3U-riq3ao";
 $completo="https://api.telegram.org/bot".$token;
 $updates=file_get_contents("php://input");
@@ -48,25 +45,20 @@ if ($isNewRequest === false || $isNewRequest === null){
 elseif(!$update){
   exit;
 }
-//msg
 $messaggio=$update['message'];
 $message_id=$update['message']['message_id'];
 $testo=$messaggio['text'];
-//utente
 $utente=$messaggio['chat']['id'];
 $utente=$messaggio['chat']['id'];
-$nomeutente=$messaggio['chat']['first_name'];
-//query msg
-$query = $update['callback_query'];
-$queryid = $query['id'];
-$queryUserId = $query['from']['id'];
-$queryusername = $query['from']['username'];
-$querydata = $query['data'];
-$querymsgid = $query['message']['message_id'];
-//data
 $datazioneunix=$messaggio['date'];
 $dataoggi = getdataoggi($datazioneunix);
-
+$nomeutente=$messaggio['chat']['first_name'];
+  $query = $update['callback_query'];
+  $queryid = $query['id'];
+  $queryUserId = $query['from']['id'];
+  $queryusername = $query['from']['username'];
+  $querydata = $query['data'];
+  $querymsgid = $query['message']['message_id'];
 
 switch($testo){
   case '/start':
