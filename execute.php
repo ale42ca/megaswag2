d7e678027b3f5852ec271a9bb33b10941385217a
ale42ca <59265646+ale42ca@users.noreply.github.com>
2020-02-06 21:31:28 +0100

    Update execute.php

--- execute.php
+++ execute.php
@@ -8,46 +8,7 @@ $completo="https://api.telegram.org/bot".$token;
 $updates=file_get_contents("php://input");
 $update=json_decode($updates, true);
 
-$updates = json_decode(file_get_contents(set_get_updates_parameters("https://api.telegram.org/bot872839539:AAGgmCXaX9zdSypFKiR4BHxoVK3U-riq3ao/getUpdates?offset=100")), true);
-function is_new_request($requestUpdateId)
-{
-    $filename = "./last_update_id.txt";
-    if (filesize($filename)) {
-        $file = fopen($filename, "w");
-        if ($file) {
-            fwrite($file, $requestUpdateId);
-            fclose($file);
-            return true;
-        } else
-            return null;
-    } else {
-        $file = fopen($filename, "w");
-        fwrite($file, 1);
-        fclose($file);
-        return false;
-    }
-}
-function set_get_updates_parameters($getUpdates)
-{
-    $filename = "./last_update_id.txt";
-    if (file_exists($filename)) {
-        $file = fopen($filename, "r");
-        $lastUpdateId = fgets($file);
-        fclose($file);
-    } else {
-        $file = fopen($filename, "w");
-        $lastUpdateId = fwrite($file, 1);
-        fclose($file);
-    }
-    return str_replace("100", $lastUpdateId, $getUpdates);
-}
-$updates = json_decode(file_get_contents(set_get_updates_parameters("https://api.telegram.org/bot872839539:AAGgmCXaX9zdSypFKiR4BHxoVK3U-riq3ao/getUpdates?offset=100")), true);
-// Separate every update in $updates
-$isNewRequest = is_new_request($update["update_id"]); // $update["update_id"] is update_id of one of your requests; e.g. 591019242
-if ($isNewRequest === false || $isNewRequest === null){
-	exit;
-	}
-elseif(!$update){
+if(!$update){
   exit;
 }
 //msg
