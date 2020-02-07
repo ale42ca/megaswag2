2c0ef3908523b11c18d942fd97c60423e235bc00
ale42ca <59265646+ale42ca@users.noreply.github.com>
2020-02-07 17:22:34 +0100

    Update execute.php

--- execute.php
+++ execute.php
@@ -68,10 +68,7 @@ $tabella= letturedatabase("SELECT COUNT(*) FROM utenti WHERE utente='$username'"
 			mandamessaggiutente($utente, $GLOBALS['utenterfl']['nomevero']);
 			if(empty($GLOBALS['utenterfl']['password'])){
 				mandamessaggiutente($utente, " dammi una passwpord inserendo /password latuapassword");
-					$msg="/password";
-					  $url = $GLOBALS[completo]."/sendMessage?chat_id=".$utente."&text=".urlencode($msg);
-					  file_get_contents($url);
-				
+					
 				
 
 			}
